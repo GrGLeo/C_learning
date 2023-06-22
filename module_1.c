dad211e6458451d069bc20cb6fe7d866b317efae
Léo Graziani <103998540+GrGLeo@users.noreply.github.com>
2023-06-22 17:38:28 +0200

    Update module_1.c

--- module_1.c
+++ module_1.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    int var1 = 10;
+
+    int var1 = 10; /*instanciate var1 with value of 10*/
+    var1 = var1 + 2;
     printf("The value of var1 is: %d \n",var1);
     return 0;
 }
