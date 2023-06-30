2113b7045c51b34f5ed0feb76a227c5f2dcf2ae9
Léo Graziani <103998540+GrGLeo@users.noreply.github.com>
2023-06-30 17:51:40 +0200

    Update module_3.c

--- module_3.c
+++ module_3.c
@@ -20,3 +20,28 @@ int main()
 	}
 	return 0;
 }
+
+int main()
+{
+	int number;
+	scanf("%d",&number);
+
+	if(number<=1){
+		return 0;
+	}
+
+	if(number>20){
+		number=20;
+	}
+
+	for(int i=0; i<number*2; i++){
+		int space = i < number ? i: (number*2 - 1) - i;
+		printf("*");
+		for(int j=0; j<space; j++){
+			print(" ");
+		}
+		printf("*\n");
+	}
+	
+	return 0;
+}
